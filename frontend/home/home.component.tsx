3c44b19f266f9076b088c3ce5974fa0a7ee09b49
Joel Denning <joeldenning@gmail.com>
2019-03-29 19:40:27 -0600

    Adding placeholder routes for recording client visits and view clients

--- frontend/home/home.component.tsx
+++ frontend/home/home.component.tsx
@@ -3,8 +3,9 @@ import PageHeader from '../page-header.component'
 import HomeCard from './home-card.component'
 import addClientIconUrl from '../../icons/148705-essential-collection/svg/add-1.svg'
 import reportIssueUrl from '../../icons/148705-essential-collection/svg/info.svg'
+import recordVisitUrl from '../../icons/148705-essential-collection/svg/hourglass-2.svg'
+import clientListUrl from '../../icons/148705-essential-collection/svg/agenda.svg'
 import {useCss} from 'kremling'
-import { mediaMobile } from '../styleguide.component';
 
 export default function Home(props: HomeProps) {
   const scope = useCss(css)
@@ -17,6 +18,16 @@ export default function Home(props: HomeProps) {
           iconUrl={addClientIconUrl}
           title="Add a new client"
           link="add-client"
+        />
+        <HomeCard
+          iconUrl={recordVisitUrl}
+          title="Record a client visit"
+          link="record-client-visit"
+        />
+        <HomeCard
+          iconUrl={clientListUrl}
+          title="View clients"
+          link="client-list"
         />
         <HomeCard
           iconUrl={reportIssueUrl}
