0d1d6e8c9a92d562b7f4a54ded9e6214aee3e703
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-05-16 05:56:21 -0600

    rebase

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,7 @@ require("./apis/clients/add-client.api");
 require("./apis/clients/client-duplicates.api");
 require("./apis/clients/get-client.api");
 require("./apis/services/list-services.api");
+require("./apis/clients/list-clients.api");
 require("./index-html.js");
 
 process.on("uncaughtException", function(err) {
