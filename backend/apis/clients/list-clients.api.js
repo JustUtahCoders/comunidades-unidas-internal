31a4c334f3d5466debac6beec1b24d9b6e00025e
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-05-19 13:18:39 -0600

    api query created

--- backend/apis/clients/list-clients.api.js
+++ backend/apis/clients/list-clients.api.js
@@ -1,4 +1,4 @@
-const { app, databaseError, pool, invalidRequiest } = require("../../server");
+const { app, databaseError, pool, invalidRequest } = require("../../server");
 const mysql = require("mysql");
 
 app.get("/api/clients", (req, res, next) => {
@@ -6,18 +6,37 @@ app.get("/api/clients", (req, res, next) => {
     if (err) {
       return databaseError(req, res, err);
     }
-    var queryString = "SELECT ? AS result";
 
-    const query = mysql.format(queryString, [req.query.foo]);
+    var queryString =
+      "SELECT cl.id,cl.firstName,cl.lastName,cl.birthday,ct.zip,ct.primaryPhone,cl.addedBy as addedById,us.firstName as addedByFirstName,us.lastname as addedByLastName,cl.dateAdded FROM clients cl JOIN contactInformation ct ON cl.id = ct.clientId JOIN users us ON cl.addedBy = us.Id WHERE (cl.firstName LIKE ? AND cl.lastName LIKE ?) OR ct.zip = ? ORDER BY cl.firstName,cl.lastName DESC LIMIT 100";
+    if (req.query.page) {
+      queryString += "," + parseInt(req.query.page) * 100;
+    }
+    const getClientList = mysql.format(queryString, [
+      req.query.firstName + "%",
+      req.query.lastName + "%",
+      req.query.zip
+    ]);
 
-    connection.query(query, function(err, rows, fields) {
+    connection.query(getClientList, function(err, rows, fields) {
       if (err) {
         return databaseError(req, res, err);
       }
       res.send({
         numClients: rows.length,
         clients: rows.map(row => ({
-          result: row.result
+          id: row.id,
+          firstName: row.firstName,
+          lastName: row.lastName,
+          birthday: row.birthday,
+          zip: row.zip,
+          primaryPhone: row.primaryPhone,
+          addedBy: {
+            addedById: row.addedById,
+            addedByFirstName: row.addedByFirstName,
+            addedByLastName: row.addedByLastName
+          },
+          dateAdded: row.dateAdded
         }))
       });
     });
