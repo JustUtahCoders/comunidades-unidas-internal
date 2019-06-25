4b706873aed43a204c3158f0ed1a22e154a34831
Meghan Bomberger <meghanbomberger@gmail.com>
2019-06-24 21:18:45 -0600

    minor prune

--- frontend/report-issue/report-issue-success.component.tsx
+++ frontend/report-issue/report-issue-success.component.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import GithubIssueSuccess from "../util/github-issue-success.component";
-import { navigate } from "@reach/router";
-import easyFetch from "../util/easy-fetch";
 
 export default function ReportIssueSuccess(props) {
   return (
