af6a750bc1937a12712331800d33542daf711fd0
Joel Denning <joeldenning@gmail.com>
2019-07-06 08:42:06 -0600

    Self review

--- backend/apis/login.api.js
+++ backend/apis/login.api.js
@@ -12,85 +12,69 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-passport.use({
-  name: "fake",
-  authenticate(req) {
-    console.log("success");
-    this.success({
-      id: 1,
-      googleProfile: {},
-      fullName: "CU Developers",
-      firstName: "CU",
-      lastName: "Developers",
-      email: "developers@cuutah.org",
-      token: "456787656789"
-    });
-  }
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    (token, refreshToken, profile, done) => {
+      pool.getConnection((err, connection) => {
+        if (err) {
+          done(err);
+        } else {
+          connection.query(
+            mysql.format(
+              `
+            INSERT IGNORE INTO users (googleId, firstName, lastName, email, accessLevel)
+            VALUES(?, ?, ?, ?, ?)
+          `,
+              [
+                profile.id,
+                profile.name.givenName,
+                profile.name.familyName,
+                profile.emails[0].value,
+                "Staff" // Start them off as staff, upgrade their access level later
+              ]
+            ),
+            (err, result) => {
+              if (err) {
+                connection.release();
+                done(err);
+              } else {
+                const getUserQuery = mysql.format(
+                  `
+                SELECT * FROM users WHERE googleId = ?
+              `,
+                  [profile.id]
+                );
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: process.env.GOOGLE_CALLBACK_URL
-//     },
-//     (token, refreshToken, profile, done) => {
-//       pool.getConnection((err, connection) => {
-//         if (err) {
-//           done(err);
-//         } else {
-//           connection.query(
-//             mysql.format(
-//               `
-//             INSERT IGNORE INTO users (googleId, firstName, lastName, email, accessLevel)
-//             VALUES(?, ?, ?, ?, ?)
-//           `,
-//               [
-//                 profile.id,
-//                 profile.name.givenName,
-//                 profile.name.familyName,
-//                 profile.emails[0].value,
-//                 "Staff" // Start them off as staff, upgrade their access level later
-//               ]
-//             ),
-//             (err, result) => {
-//               if (err) {
-//                 connection.release();
-//                 done(err);
-//               } else {
-//                 const getUserQuery = mysql.format(
-//                   `
-//                 SELECT * FROM users WHERE googleId = ?
-//               `,
-//                   [profile.id]
-//                 );
+                connection.query(getUserQuery, (err, rows) => {
+                  connection.release();
 
-//                 connection.query(getUserQuery, (err, rows) => {
-//                   connection.release();
-
-//                   if (err) {
-//                     done(err);
-//                   } else {
-//                     done(null, {
-//                       id: rows[0].id,
-//                       googleProfile: profile,
-//                       fullName: rows[0].firstName + " " + rows[0].lastName,
-//                       firstName: rows[0].firstName,
-//                       lastName: rows[0].lastName,
-//                       email: rows[0].email,
-//                       token: token
-//                     });
-//                   }
-//                 });
-//               }
-//             }
-//           );
-//         }
-//       });
-//     }
-//   )
-// );
+                  if (err) {
+                    done(err);
+                  } else {
+                    done(null, {
+                      id: rows[0].id,
+                      googleProfile: profile,
+                      fullName: rows[0].firstName + " " + rows[0].lastName,
+                      firstName: rows[0].firstName,
+                      lastName: rows[0].lastName,
+                      email: rows[0].email,
+                      token: token
+                    });
+                  }
+                });
+              }
+            }
+          );
+        }
+      });
+    }
+  )
+);
 
 app.use(
   cookieSession({
@@ -105,20 +89,11 @@ app.use(passport.initialize());
 
 app.get(
   "/login",
-  passport.authenticate("fake"),
-  (req, res) => {
-    res.redirect("/");
-  }
-  // passport.initialize("fake", {}),
-  // (req, res) => {
-  //   console.log('req.session', req.session)
-  //   res.redirect('/')
-  // }
-  // passport.authenticate("google", {
-  //   scope: ["profile", "email"],
-  //   includeGrantedScopes: true,
-  //   hd: "cuutah.org"
-  // })
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    includeGrantedScopes: true,
+    hd: "cuutah.org"
+  })
 );
 
 app.get(
@@ -145,6 +120,6 @@ app.get(
 app.get("/logout", (req, res) => {
   req.logout();
   req.session = null;
-  res.cookie("session", "", { maxAge: 1 });
+  res.cookie("user", "", { maxAge: 1 });
   res.redirect("/login/select-account");
 });
