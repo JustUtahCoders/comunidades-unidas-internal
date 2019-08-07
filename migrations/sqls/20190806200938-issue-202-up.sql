65490643fa4f1510962f7953596da7db2b2d1404
Joel Denning <joeldenning@gmail.com>
2019-08-07 16:45:52 -0600

    More adjustments to programs/services list. Resolves #202. (#205)
    * More adjustments to programs/services list. Resolves #202.
    
    * Meghan's feedback

---
+++ migrations/sqls/20190806200938-issue-202-up.sql
@@ -0,0 +1,41 @@
+/* Replace with your SQL commands */
+
+UPDATE services
+SET programId = (
+  SELECT id FROM programs WHERE programName = "Community Engagement and Organizing"
+)
+WHERE serviceName = "Leadership Development - Monthly Meetings";
+
+INSERT INTO services (serviceName, serviceDesc, programId)
+VALUES
+  ("General Consultation", "General Consultation",
+    (SELECT id FROM programs WHERE programName = "Immigration")
+  ),
+  ("Translation", "Translation", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("Green Card Renewal", "Green Card Renewal", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("I-821 Temporary Protected Status (TPS)", "I-821 Temporary Protected Status (TPS)", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("I-765 Application for Employment Authorization", "I-765 Application for Employment Authorization", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("Freedom of Information Act (FOIA)", "Freedom of Information Act (FOIA)", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("Status Check", "Status Check", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("I-94 Request", "I-94 Request", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  )),
+  ("Family Petition - Consular Processing", "Family Petition - Consular Processing", (
+    SELECT id FROM programs WHERE programName = "Immigration"
+  ));
+
+UPDATE services
+SET serviceName = "Family Petition - Adjustment of Status"
+WHERE serviceName = "Family Petition";
