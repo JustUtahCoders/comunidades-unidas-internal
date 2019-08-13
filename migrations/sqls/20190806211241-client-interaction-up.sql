36286617173374612eca30c6d52320fbdeb2d3cd
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-08-13 15:21:28 -0600

    Update 20190806211241-client-interaction-up.sql

--- migrations/sqls/20190806211241-client-interaction-up.sql
+++ migrations/sqls/20190806211241-client-interaction-up.sql
@@ -11,19 +11,19 @@ CREATE TABLE IF NOT EXISTS clientInteractions (
     ) NOT NULL,
     date DATE DEFAULT CURRENT_DATE NOT NULL,
     title ENUM (
-        'Client interaction created - Preventive Health: Chronic Disease Screenings',
-        'Client interaction created - Nutrition / CRYS / SNAP: SNAP',
-        'Client interaction created - Nutrition / CRYS / SNAP: Nutrition',
-        'Client interaction created - Nutrition / CRYS / SNAP: Grocery Store Tour',
-        'Client interaction created - Nutrition / CRYS / SNAP: Cooking Classes',
-        'Client interaction created - Immigration: Citizenship',
-        'Client interaction created - Immigration: Family Petition',
-        'Client interaction created - Immigration: DACA',
-        'Client interaction created - Financial Education: Financial Coach',
-        'Client interaction created - Financial Education: Financial Education',
-        'Client interaction created - Workers' Rights: Workers' Rights and Safety',
-        'Client interaction created - Community Engagement and Organizing: Leadership Development',
-        'Client interaction created - Community Engagement and Organizing: Youth Groups'
+        'Client interaction created for Preventive Health: Chronic Disease Screenings',
+        'Client interaction created for Nutrition / CRYS / SNAP: SNAP',
+        'Client interaction created for Nutrition / CRYS / SNAP: Nutrition',
+        'Client interaction created for Nutrition / CRYS / SNAP: Grocery Store Tour',
+        'Client interaction created for Nutrition / CRYS / SNAP: Cooking Classes',
+        'Client interaction created for Immigration: Citizenship',
+        'Client interaction created for Immigration: Family Petition',
+        'Client interaction created for Immigration: DACA',
+        'Client interaction created for Financial Education: Financial Coach',
+        'Client interaction created for Financial Education: Financial Education',
+        'Client interaction created for Workers' Rights: Workers' Rights and Safety',
+        'Client interaction created for Community Engagement and Organizing: Leadership Development',
+        'Client interaction created for Community Engagement and Organizing: Youth Groups'
     ),
     duration TIME DEFAULT '0:00:00' NOT NULL,
     location ENUM (
@@ -41,3 +41,18 @@ CREATE TABLE IF NOT EXISTS clientInteractions (
     FOREIGN KEY (modifiedBy) REFERENCES users(id),
     FOREIGN KEY (serviceId) REFERENCES services(id)
 );
+
+ALTER TABLE clientLogs
+    MODIFY COLUMN
+        logType enum (
+            'clientCreated',
+            'clientUpdated:basicInformation',
+            'clientUpdated:contactInformation',
+            'clientUpdated:demographics',
+            'clientUpdated:intakeData',
+            'caseNote',
+            'clientInteractionCreated',
+            'clientInteractionUpdated',
+            'clientInteractionDeleted'
+        ) NOT NULL
+    
