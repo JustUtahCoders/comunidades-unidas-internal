38ae072e483209516e3f74e83815958ce7ffe985
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-08-12 22:12:02 -0600

    Update 20190806211241-client-interaction-up.sql

--- migrations/sqls/20190806211241-client-interaction-up.sql
+++ migrations/sqls/20190806211241-client-interaction-up.sql
@@ -1,33 +1,28 @@
 CREATE TABLE IF NOT EXISTS clientInteractions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    clientInteractionType ENUM (
-        'clientInteractionCreated',
-        'clientInteractionUpdated',
-        'clientInteractionDeleted'
-    ) NOT NULL,
     clientId INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
     serviceId INT NOT NULL,
     interactionType ENUM (
-        'in pesron',
-        'by phone',
-        'workshop/talk',
-        'one on one/light touch',
+        'inPesron',
+        'byPhone',
+        'workshopTalk',
+        'oneOnOneLightTouch',
         'consultation'
     ) NOT NULL,
-    description MEDIUMTEXT,
     date DATE DEFAULT CURRENT_DATE NOT NULL,
     duration TIME DEFAULT '0:00:00' NOT NULL,
     location ENUM (
-        'CU Office',
-        'Consulate Office',
-        'Community Event'
+        'CUOffice',
+        'consulateOffice',
+        'communityEvent'
     ) NOT NULL,
-    canModify BOOLEAN DEFAULT false NOT NULL,
     isDeleted BOOLEAN DEFAULT false NOT NULL,
     dateAdded DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     addedBy int NOT NULL,
+    dateModified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    modifiedBy int NOT NULL /*Self Reference to User ID*/, 
     FOREIGN KEY (clientId) REFERENCES clients(id),
     FOREIGN KEY (addedBy) REFERENCES users(id),
+    FOREIGN KEY (modifiedBy) REFERENCES users(id),
     FOREIGN KEY (serviceId) REFERENCES services(id)
 );
