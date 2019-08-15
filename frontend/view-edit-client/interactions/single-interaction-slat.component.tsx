d2d020d9c2314924d4f583525b28666f2a5bd89a
Joel Denning <joeldenning@gmail.com>
2019-08-14 23:54:25 -0600

    Working on frontend for client interactions. See #183. (#215)
    * Adding date and duration
    
    * Adding inputs for interaction type and date of interaction
    
    * Starting on time duration
    
    * Time duration

--- frontend/view-edit-client/interactions/single-interaction-slat.component.tsx
+++ frontend/view-edit-client/interactions/single-interaction-slat.component.tsx
@@ -1,20 +1,44 @@
 import React from "react";
 import { useCss } from "kremling";
 import { CUServicesList } from "../../add-client/services.component";
+import { groupBy } from "lodash-es";
+import dayjs from "dayjs";
+import TimeDurationInput, {
+  TimeDuration
+} from "../../util/time-duration-input.component";
 
 export default React.forwardRef<any, SingleClientInteractionProps>(
   function SingleClientInteraction(props, ref) {
     const scope = useCss(css);
+    const [selectedService, setSelectedService] = React.useState(null);
+    const [
+      selectedInteractionType,
+      setSelectedInteractionType
+    ] = React.useState(null);
+    const [dateOfInteraction, setDateOfInteraction] = React.useState(
+      dayjs().format("YYYY-MM-DD")
+    );
+    const [duration, setDuration] = React.useState<TimeDuration>({
+      hours: 0,
+      minutes: 30,
+      stringValue: "00:30"
+    });
+    const [selectedLocation, setSelectedLocation] = React.useState(
+      interactionLocations.CUOffice
+    );
 
     const services = props.servicesResponse
       ? props.servicesResponse.services
       : [];
 
+    const groupedServices = groupBy(services, "programName");
+
     return (
       <div className="single-client-interaction" {...scope}>
         <div className="header">
           <h3 className="interaction-number">
-            Interaction {props.interactionIndex + 1}
+            #{props.interactionIndex + 1}
+            {selectedService ? ` ${selectedService}` : ""}
           </h3>
           {props.interactionIndex > 0 && (
             <button
@@ -26,13 +50,75 @@ export default React.forwardRef<any, SingleClientInteractionProps>(
             </button>
           )}
         </div>
-        <select ref={ref}>
-          {services.map(service => (
-            <option key={service.id}>{service.serviceName}</option>
-          ))}
-        </select>
+        <div className="inputs">
+          <label id={`provided-service-${props.interactionIndex}`}>
+            Service:
+          </label>
+          <select
+            ref={ref}
+            value={selectedService || ""}
+            onChange={evt => setSelectedService(evt.target.value)}
+            aria-labelledby={`provided-service-${props.interactionIndex}`}
+            className="services-select"
+          >
+            {Object.keys(groupedServices).map(programName => (
+              <optgroup label={programName} key={programName}>
+                {groupedServices[programName].map(service => (
+                  <option key={service.id}>{service.serviceName}</option>
+                ))}
+              </optgroup>
+            ))}
+          </select>
+          <label id={`interaction-type-${props.interactionIndex}`}>Type:</label>
+          <select
+            value={selectedInteractionType || ""}
+            onChange={evt => setSelectedInteractionType(evt.target.value)}
+            aria-labelledby={`interaction-type-${props.interactionIndex}`}
+          >
+            {Object.keys(interactionTypes).map(interactionType => (
+              <option key={interactionType} value={interactionType}>
+                {interactionTypes[interactionType]}
+              </option>
+            ))}
+          </select>
+          <label id={`interaction-date-${props.interactionIndex}`}>Date:</label>
+          <input
+            type="date"
+            value={dateOfInteraction}
+            onChange={evt => setDateOfInteraction(evt.target.value)}
+            aria-labelledby={`interaction-date-${props.interactionIndex}`}
+          />
+          <label id={`interaction-duration-${props.interactionIndex}`}>
+            Duration:
+          </label>
+          <TimeDurationInput
+            labelId={`interaction-duration-${props.interactionIndex}`}
+            initialValue="00:30"
+            setValue={handleTimeChange}
+          />
+          {selectedInteractionType !== "byPhone" && (
+            <>
+              <label id={`interaction-location-${props.interactionIndex}`}>
+                Location:
+              </label>
+              <select
+                value={selectedLocation || ""}
+                onChange={evt => setSelectedLocation(evt.target.value)}
+                aria-labelledby={`interaction-location-${props.interactionIndex}`}
+              >
+                {Object.keys(interactionLocations).map(location => (
+                  <option key={location} value={location}>
+                    {interactionLocations[location]}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
+        </div>
       </div>
     );
+
+    function handleTimeChange() {}
   }
 );
 
@@ -57,7 +143,40 @@ const css = `
 & .header button {
   cursor: pointer;
 }
+
+& .inputs {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  row-gap: 1.6rem;
+  column-gap: 1.6rem;
+}
+
+& .inputs input {
+  width: min-content;
+}
+
+& .inputs label {
+  text-align: right;
+}
+
+& .services-select {
+  min-width: 100%;
+}
 `;
+
+export const interactionTypes = {
+  inPerson: "In Person",
+  byPhone: "By Phone",
+  workshopTalk: "Workshop / Talk",
+  oneOnOneLightTough: "One On One / Light Tough",
+  consultation: "Consultation"
+};
+
+export const interactionLocations = {
+  CUOffice: "CU Office",
+  consulate: "Consulate",
+  communityEvent: "Community Event"
+};
 
 type SingleClientInteractionProps = {
   servicesResponse: CUServicesList;
