df75db800cfb5f9af8dbf37489a9e3bc2c5eb9c8
Meghan Bomberger <meghanbomberger@gmail.com>
2019-08-27 15:39:03 -0600

    migration for delete client feature

---
+++ migrations/sqls/20190827213018-delete-client-down.sql
@@ -0,0 +1,18 @@
+ALTER TABLE clients
+	DROP COLUMN
+		isDeleted;
+
+ALTER TABLE clientLogs
+    MODIFY COLUMN
+        logType enum (
+            'clientCreated',
+            'clientUpdated:basicInformation',
+            'clientUpdated:contactInformation',
+            'clientUpdated:demographics',
+            'clientUpdated:intakeData',
+            'caseNote',
+            'clientInteraction:created',
+            'clientInteraction:updated',
+            'clientInteraction:deleted',
+            'clientInteraction:serviceProvided'
+        ) NOT NULL;   
---
+++ migrations/sqls/20190827213018-delete-client-up.sql
@@ -0,0 +1,19 @@
+ALTER TABLE clients
+	ADD COLUMN
+		isDeleted BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE clientLogs
+    MODIFY COLUMN
+        logType enum (
+            'clientCreated',
+            'clientUpdated:basicInformation',
+            'clientUpdated:contactInformation',
+            'clientUpdated:demographics',
+            'clientUpdated:intakeData',
+			'clientDeleted',
+            'caseNote',
+            'clientInteraction:created',
+            'clientInteraction:updated',
+            'clientInteraction:deleted',
+            'clientInteraction:serviceProvided'
+        ) NOT NULL;   
