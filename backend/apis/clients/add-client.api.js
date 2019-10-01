61f26438396844c6809d852f023ffe7e7f6a7dd3
Meghan Bomberger <meghanbomberger@gmail.com>
2019-10-01 14:02:46 -0600

    confirm working

--- backend/apis/clients/add-client.api.js
+++ backend/apis/clients/add-client.api.js
@@ -13,6 +13,7 @@ const {
   validZip,
   nullableValidEmail,
   nullableValidEnum,
+  nullableValidInteger,
   validEnum,
   validCountry,
   validArray,
@@ -69,7 +70,7 @@ app.post("/api/clients", (req, res, next) => {
         "every-year"
       ),
       nullableValidEnum("weeklyEmployedHours", "0-20", "21-35", "36-40", "41+"),
-      validInteger("householdIncome"),
+      nullableValidInteger("householdIncome"),
       validInteger("householdSize"),
       validBoolean("isStudent"),
       validEnum("housingStatus", "renter", "homeowner", "other"),
