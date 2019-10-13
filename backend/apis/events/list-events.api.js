c53ab2e41a622ac37625e747068cfb1c3b01ab70
Meghan Bomberger <meghanbomberger@gmail.com>
2019-10-13 15:30:17 -0600

    progress update

--- backend/apis/events/list-events.api.js
+++ backend/apis/events/list-events.api.js
@@ -2,7 +2,26 @@ const { app, databaseError, pool } = require("../../server");
 const mysql = require("mysql");
 
 app.get("/api/events", (req, res, next) => {
-  const getEvents = mysql.format(`SELECT * FROM events;`);
+  const getEvents = mysql.format(`
+    SELECT
+      events.id AS eventId,
+      events.eventName,
+      events.eventDate,
+      events.eventLocation,
+      events.totalAttendence,
+      events.isDeleted,
+      events.dateAdded,
+      events.dateModified,
+      events.addedBy AS createdByUserId,
+      events.modifiedBy AS modifiedByUserId,
+      created.firstName AS createdByFirstName,
+      created.lastName AS createdByLastName,
+      modified.firstName AS modifiedByFirstName,
+      modified.lastName AS modifiedByLastName
+    FROM events
+    INNER JOIN users created ON created.id = events.addedBy
+    INNER JOIN users modified ON modified.id = events.modifiedBy;
+  `);
 
   pool.query(getEvents, (err, results) => {
     if (err) {
@@ -11,6 +30,32 @@ app.get("/api/events", (req, res, next) => {
 
     const [events] = results;
 
-    res.send(results);
+    res.send({
+      events: events.map(e => ({
+        id: e.eventId,
+        eventName: e.eventName,
+        eventDate: responseDateWithoutTime(e.eventDate),
+        eventLocation: e.eventLocation,
+        totalAttendence: e.totalAttendence,
+        isDeleted: responseBoolean(e.isDeleted),
+        createdBy: {
+          userId: e.createdByUserId,
+          firstName: e.createdByFirstName,
+          lastName: e.createdByLastName,
+          fullName: responseFullName(e.createdByFirstName, e.createdByLastName),
+          timestamp: e.dateAdded
+        },
+        lastUpdatedBy: {
+          userId: e.modifiedByUserId,
+          firstName: e.modifiedByFirstName,
+          lastName: e.modifiedByLastName,
+          fullName: responseFullName(
+            e.modifiedByFirstName,
+            e.modifiedByLastName
+          ),
+          timestamp: e.dateModified
+        }
+      }))
+    });
   });
 });
