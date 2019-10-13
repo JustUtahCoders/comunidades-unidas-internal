ff225104f141e18a2386a092fcfce23e0ca25d1e
Meghan Bomberger <meghanbomberger@gmail.com>
2019-10-12 21:28:10 -0600

    progress update

---
+++ backend/apis/events/list-events.api.js
@@ -0,0 +1,38 @@
+const { app, databaseError, pool } = require("../../server");
+const mysql = require("mysql");
+
+app.get("/api/services", (req, res, next) => {
+  const getServices = mysql.format(`
+    SELECT *, services.id AS serviceId, programs.id AS programId
+    FROM services JOIN programs WHERE services.programId = programs.id;
+    SELECT * FROM programs;
+  `);
+
+  pool.query(getServices, (err, results) => {
+    if (err) {
+      return databaseError(req, res, err);
+    }
+
+    const [services, programs] = results;
+
+    const programMap = programs.reduce((acc, program) => {
+      acc[program.id] = program;
+      return acc;
+    }, {});
+
+    res.send({
+      services: services.map(s => ({
+        id: s.serviceId,
+        serviceName: s.serviceName,
+        serviceDescription: s.serviceDesc,
+        programId: s.programId,
+        programName: programMap[s.programId].programName
+      })),
+      programs: programs.map(p => ({
+        id: p.id,
+        programName: p.programName,
+        programDescription: p.programDescription
+      }))
+    });
+  });
+});
