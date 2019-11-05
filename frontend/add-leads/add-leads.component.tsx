2a913636635cd7d160a8c26e081f78c6dacf9f3b
Joel Denning <joeldenning@gmail.com>
2019-11-05 15:41:49 -0700

    Beginnings of Add Leads (#367)
    * WIP - bulk add leads
    
    Not much here, just some prototyping... playing around with an editable
    grid type thing
    
    * Beginnings of Add Leads UI
    
    * Self review
    
    * Meghan's feedback

---
+++ frontend/add-leads/add-leads.component.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import AddEventStep from "./add-event-step.component";
+import AddLeadsStep from "./add-leads-step.component";
+import ReportIssue from "../report-issue/report-issue.component";
+import PageHeader from "../page-header.component";
+
+export default function AddLeads(props: AddLeadsProps) {
+  const [state, dispatch] = React.useReducer(reducer, { step: Step.addEvent });
+  const featureEnabled = localStorage.getItem("leads");
+  const StepComponent = StepComponents[Step[state.step]];
+
+  let content: JSX.Element = null;
+  if (!featureEnabled) {
+    content = <ReportIssue missingFeature hideHeader />;
+  } else if (Step) {
+    content = <StepComponent />;
+  }
+
+  return (
+    <>
+      <PageHeader title="Add new leads" />
+      {content}
+    </>
+  );
+}
+
+function reducer(state: AddLeadsState, action: AddLeadsAction) {
+  switch (action.type) {
+    case ActionTypes.changeStep:
+      return { ...state, step: action.step };
+    default:
+      throw Error();
+  }
+}
+
+type AddLeadsState = {
+  step: Step;
+};
+
+type AddLeadsProps = {
+  path: string;
+};
+
+type AddLeadsAction = ChangeStep;
+
+type ChangeStep = {
+  type: ActionTypes.changeStep;
+  step: Step;
+};
+
+enum ActionTypes {
+  changeStep = "changeStep"
+}
+
+enum Step {
+  addEvent = "addEvent",
+  addLeads = "addLeads"
+}
+
+const StepComponents = {
+  [Step.addEvent]: AddEventStep,
+  [Step.addLeads]: AddLeadsStep
+};
