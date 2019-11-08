a1dcc7909d035959dafbc9060f6717d03e426323
Meghan Bomberger <meghanbomberger@gmail.com>
2019-11-08 16:59:39 -0700

    catch up with master

--- frontend/leads-list/toolbar/leads-table-toolbar.component.tsx
+++ frontend/leads-list/toolbar/leads-table-toolbar.component.tsx
@@ -5,9 +5,6 @@ import { mediaDesktop, mediaMobile } from "../../styleguide.component";
 import backIcon from "../../../icons/148705-essential-collection/svg/back.svg";
 import nextIcon from "../../../icons/148705-essential-collection/svg/next.svg";
 
-//FIXME
-import TestUpdateLeads from "./test-lead-update.component";
-
 export default function LeadsTableToolbar(props: LeadsTableToolbarProps) {
   const scope = useCss(css);
 
@@ -16,10 +13,6 @@ export default function LeadsTableToolbar(props: LeadsTableToolbarProps) {
   return (
     <div className="leads-table-toolbar" {...scope}>
       <div className="desktop-table-toolbar">
-        {/* FIXME */}
-        <TestUpdateLeads />
-        {/* FIXME */}
-
         {lastPage !== 0 && (
           <div className="pagination-container">
             <div>
