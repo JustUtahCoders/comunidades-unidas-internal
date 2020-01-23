a223466d4467876622e96b1deceac24c68929116
Joel Denning <joeldenning@gmail.com>
2020-01-23 14:52:16 -0700

    Adding max interaction seconds to report

--- frontend/reports/interaction-hours-by-client/interaction-hours-by-client-params.component.tsx
+++ frontend/reports/interaction-hours-by-client/interaction-hours-by-client-params.component.tsx
@@ -3,11 +3,17 @@ import { useQueryParamState } from "../../util/use-query-param-state.hook";
 import { Link } from "@reach/router";
 
 const sixHoursInSeconds = 21600;
+const tenThousandHoursInSeconds = 10000 * 60 * 60;
 
 export default function InteractionHoursByClientParams(props) {
   const [minInteractionSeconds, setMinInteractionSeconds] = useQueryParamState(
     "minInteractionSeconds",
     sixHoursInSeconds.toString()
+  );
+
+  const [maxInteractionsSeconds, setMaxInteractionSeconds] = useQueryParamState(
+    "maxInteractionSeconds",
+    tenThousandHoursInSeconds.toString()
   );
 
   return (
@@ -16,11 +22,28 @@ export default function InteractionHoursByClientParams(props) {
         <label id="min-hours-label">Minimum # of interaction hours:</label>
         <input
           type="number"
+          step=".1"
           value={Number(minInteractionSeconds) / 3600}
           onChange={evt =>
-            setMinInteractionSeconds(String(Number(evt.target.value) * 3600))
+            setMinInteractionSeconds(
+              String(Math.round(Number(evt.target.value) * 3600))
+            )
           }
           aria-labelledby="min-hours-label"
+        />
+      </div>
+      <div className="report-input">
+        <label id="max-hours-label">Maximum # of interaction hours:</label>
+        <input
+          type="number"
+          step=".1"
+          value={Number(maxInteractionsSeconds) / 3600}
+          onChange={evt =>
+            setMaxInteractionSeconds(
+              String(Math.round(Number(evt.target.value) * 3600))
+            )
+          }
+          aria-labelledby="max-hours-label"
         />
       </div>
       <div className="actions">
