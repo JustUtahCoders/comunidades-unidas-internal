f8cf9bd21e45456129be37c15428cfadfdd2bc99
Joel Denning <joeldenning@gmail.com>
2020-02-01 18:49:38 -0700

    Reducing bundle size by removing react-axe from prod build

--- frontend/comunidades-unidas-internal.tsx
+++ frontend/comunidades-unidas-internal.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root.component";
-import axe from "react-axe";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
 if (process.env.NODE_ENV !== "production") {
+  const axe = require("react-axe");
   axe(React, ReactDOM, 1000);
 }
