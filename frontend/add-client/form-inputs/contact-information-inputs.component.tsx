74bed53106b754e1be25ad93cca446861d75ab29
Joel Denning <joeldenning@gmail.com>
2020-03-15 13:17:35 -0600

    Removing console log

--- frontend/add-client/form-inputs/contact-information-inputs.component.tsx
+++ frontend/add-client/form-inputs/contact-information-inputs.component.tsx
@@ -9,7 +9,6 @@ export default React.forwardRef(function ContactInformationInputs(
   props: ContactInformationInputsProps,
   ref
 ) {
-  console.log(props.client);
   const [phone, setPhone] = React.useState(props.client.phone || "");
   const [smsConsent, setSmsConsent] = React.useState(
     props.client.smsConsent || false
