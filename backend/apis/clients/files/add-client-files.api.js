5142dcf44efedcaea16149f96eab6a5c639bc011
Joel Denning <joeldenning@gmail.com>
2020-05-19 14:29:46 -0600

    Listing client files is now working.

--- backend/apis/clients/files/add-client-files.api.js
+++ backend/apis/clients/files/add-client-files.api.js
@@ -12,6 +12,7 @@ const {
   nonEmptyString,
   validInteger,
 } = require("../../utils/validation-utils");
+const { responseFullName } = require("../../utils/transform-utils");
 const { Bucket } = require("./file-helpers");
 const mysql = require("mysql");
 
@@ -31,53 +32,80 @@ app.post("/api/clients/:clientId/files", (req, res) => {
     return invalidRequest(res, validationErrors);
   }
 
+  const clientId = req.params.clientId;
   const user = req.session.passport.user;
 
-  AWS.config.getCredentials((err, data) => {
+  const checkClientSql = mysql.format(
+    `
+    SELECT isDeleted FROM clients WHERE id = ?;
+  `,
+    [clientId]
+  );
+
+  pool.query(checkClientSql, (err, checkClientResult) => {
     if (err) {
-      return internalError(req, res, error);
+      return databaseError(err);
     }
 
-    const s3 = new AWS.S3();
-    s3.headObject(
-      {
-        Bucket,
-        Key: req.body.s3Key,
-      },
-      (err, data) => {
-        if (err) {
-          return internalError(req, res, error);
-        }
+    if (checkClientResult.length === 0) {
+      return res
+        .status(404)
+        .send({ errors: ["No such client with id " + clientId] });
+    }
 
-        const insertSql = mysql.format(
-          `
-        INSERT INTO clientFiles (s3Key, fileName, fileSize, fileExtension, addedBy)
-        VALUES (?, ?, ?, ?, ?);
+    AWS.config.getCredentials((err, data) => {
+      if (err) {
+        return internalError(req, res, error);
+      }
 
-        SELECT LAST_INSERT_ID() id;
-      `,
-          [
-            req.body.s3Key,
-            req.body.fileName,
-            req.body.fileSize,
-            req.body.fileExtension,
-            user.id,
-          ]
-        );
-
-        pool.query(insertSql, (err, result) => {
+      const s3 = new AWS.S3();
+      s3.headObject(
+        {
+          Bucket,
+          Key: req.body.s3Key,
+        },
+        (err, data) => {
           if (err) {
-            return databaseError(req, res, err);
+            return internalError(req, res, error);
           }
 
-          res.json({
-            id: result[1][0].id,
-            s3Key: req.body.s3Key,
-            fileSize: req.body.fileSize,
-            fileExtension: req.body.fileExtension,
+          const insertSql = mysql.format(
+            `
+          INSERT INTO clientFiles (s3Key, fileName, fileSize, fileExtension, addedBy, clientId)
+          VALUES (?, ?, ?, ?, ?, ?);
+
+          SELECT LAST_INSERT_ID() id;
+        `,
+            [
+              req.body.s3Key,
+              req.body.fileName,
+              req.body.fileSize,
+              req.body.fileExtension,
+              user.id,
+              clientId,
+            ]
+          );
+
+          pool.query(insertSql, (err, result) => {
+            if (err) {
+              return databaseError(req, res, err);
+            }
+
+            res.json({
+              id: result[1][0].id,
+              s3Key: req.body.s3Key,
+              fileSize: req.body.fileSize,
+              fileExtension: req.body.fileExtension,
+              createdBy: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                fullName: responseFullName(user.firstName, user.lastName),
+                timestamp: new Date(),
+              },
+            });
           });
-        });
-      }
-    );
+        }
+      );
+    });
   });
 });
