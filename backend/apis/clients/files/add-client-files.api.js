652d190af91ceca2552e59ef6cf37700c128bb33
Joel Denning <joeldenning@gmail.com>
2020-05-19 13:38:52 -0600

    Uploading client files is now working (#521)
    * Uploading client files is now working
    
    * Beginnings of client files
    
    * Wherever i was
    
    * Putting behind feature toggle
    
    * Self review
    
    Removing credentials
    
    Uploading client files is now working.
    
    * Self review

---
+++ backend/apis/clients/files/add-client-files.api.js
@@ -0,0 +1,83 @@
+const {
+  app,
+  internalError,
+  invalidRequest,
+  databaseError,
+  pool,
+} = require("../../../server");
+const AWS = require("aws-sdk");
+const {
+  checkValid,
+  validId,
+  nonEmptyString,
+  validInteger,
+} = require("../../utils/validation-utils");
+const { Bucket } = require("./file-helpers");
+const mysql = require("mysql");
+
+app.post("/api/clients/:clientId/files", (req, res) => {
+  const validationErrors = [
+    ...checkValid(req.params, validId("clientId")),
+    ...checkValid(
+      req.body,
+      nonEmptyString("s3Key"),
+      nonEmptyString("fileName"),
+      validInteger("fileSize"),
+      nonEmptyString("fileExtension")
+    ),
+  ];
+
+  if (validationErrors.length > 0) {
+    return invalidRequest(res, validationErrors);
+  }
+
+  const user = req.session.passport.user;
+
+  AWS.config.getCredentials((err, data) => {
+    if (err) {
+      return internalError(req, res, error);
+    }
+
+    const s3 = new AWS.S3();
+    s3.headObject(
+      {
+        Bucket,
+        Key: req.body.s3Key,
+      },
+      (err, data) => {
+        if (err) {
+          return internalError(req, res, error);
+        }
+
+        const insertSql = mysql.format(
+          `
+        INSERT INTO clientFiles (s3Key, fileName, fileSize, fileExtension, addedBy)
+        VALUES (?, ?, ?, ?, ?);
+
+        SELECT LAST_INSERT_ID() id;
+      `,
+          [
+            req.body.s3Key,
+            req.body.fileName,
+            req.body.fileSize,
+            req.body.fileExtension,
+            user.id,
+          ]
+        );
+
+        pool.query(insertSql, (err, result) => {
+          if (err) {
+            return databaseError(req, res, err);
+          }
+
+          res.json({
+            id: result[1][0].id,
+            s3Key: req.body.s3Key,
+            fileSize: req.body.fileSize,
+            fileExtension: req.body.fileExtension,
+          });
+        });
+      }
+    );
+  });
+});
---
+++ backend/apis/clients/files/file-helpers.js
@@ -0,0 +1,2 @@
+exports.Bucket =
+  process.env.FILE_UPLOAD_BUCKET_NAME || "comunidades-unidas-test-file-uploads";
