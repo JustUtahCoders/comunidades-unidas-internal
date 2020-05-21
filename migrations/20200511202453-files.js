809787317e93044a85ddb4de077ada733bba282e
Joel Denning <joeldenning@gmail.com>
2020-05-21 16:24:03 -0600

    Fixing database migrations

--- migrations/20200511202453-files.js
+++ migrations/20200511202453-files.js
@@ -19,11 +19,7 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db) {
-  var filePath = path.join(
-    __dirname,
-    "sqls",
-    "20200519194054-files-fix-up.sql"
-  );
+  var filePath = path.join(__dirname, "sqls", "20200511202453-files-up.sql");
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       if (err) return reject(err);
@@ -37,11 +33,7 @@ exports.up = function (db) {
 };
 
 exports.down = function (db) {
-  var filePath = path.join(
-    __dirname,
-    "sqls",
-    "20200519194054-files-fix-down.sql"
-  );
+  var filePath = path.join(__dirname, "sqls", "20200511202453-files-down.sql");
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       if (err) return reject(err);
--- migrations/sqls/20200511202453-files-up.sql
+++ migrations/sqls/20200511202453-files-up.sql
@@ -1,11 +1,13 @@
 CREATE table clientFiles (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  s3Key VARCHAR(256),
-  fileName VARCHAR(256),
-  fileSize INT,
-  fileExtension VARCHAR(32),
+  clientId INT NOT NULL,
+  s3Key VARCHAR(256) NOT NULL,
+  fileName VARCHAR(256) NOT NULL,
+  fileSize INT NOT NULL,
+  fileExtension VARCHAR(32) NOT NULL,
   isDeleted BOOLEAN DEFAULT false,
   dateAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
-  addedBy INT,
-  FOREIGN KEY (addedBy) REFERENCES users(id)
+  addedBy INT NOT NULL,
+  FOREIGN KEY (addedBy) REFERENCES users(id),
+  FOREIGN KEY (clientId) REFERENCES clients(id)
 );
