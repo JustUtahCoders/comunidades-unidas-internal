7cce38b9be7d32b474ad05de896cca49f8d404a9
Joel Denning <joeldenning@gmail.com>
2020-06-05 12:17:34 -0600

    Fixing time ranges for interaction hours by program/service report. Resolves #554.

--- frontend/reports/interactions-by-service/interactions-by-service-results.component.tsx
+++ frontend/reports/interactions-by-service/interactions-by-service-results.component.tsx
@@ -6,6 +6,7 @@ import CollapsibleTableRows, {
   ToggleCollapseButton,
   ToggleAllButton,
 } from "../shared/collapsible-table-rows.component";
+import dayjs from "dayjs";
 
 export default function InteractionsByService(props) {
   const { isLoading, data, error } = useReportsApi(
@@ -26,54 +27,86 @@ export default function InteractionsByService(props) {
   });
 
   return (
-    <BasicTableReport
-      tableStyle={{ width: "100%" }}
-      title={`Interactions by Program and Service`}
-      headerRows={
-        <tr>
-          <th>Program</th>
-          <th style={{ width: "20%" }}>Service</th>
-          <th>Client Count</th>
-          <th>Interaction Count</th>
-          <th>Interaction Hours</th>
-        </tr>
-      }
-      contentRows={data.programs.map((program) => (
-        <CollapsibleTableRows
-          key={program.programId}
-          everpresentRow={
+    <>
+      <BasicTableReport
+        title="Interactions By Program and Service"
+        headerRows={
+          <tr>
+            <th>Parameter</th>
+            <th>Value</th>
+          </tr>
+        }
+        contentRows={
+          <>
             <tr>
-              <th>{program.programName}</th>
+              <th>Start Date</th>
               <td>
-                <ToggleCollapseButton />
+                {data.reportParameters.start
+                  ? dayjs(data.reportParameters.start).format("MMM D, YYYY")
+                  : "\u2014"}
               </td>
-              <td>{program.numClients.toLocaleString()}</td>
-              <td>{program.numInteractions.toLocaleString()}</td>
-              <td>{formatDuration(program.totalDuration)}</td>
             </tr>
-          }
-          collapsibleRows={groupedServices[program.programId].map((service) => (
-            <tr key={service.serviceId}>
-              <td>{"\u2014"}</td>
-              <th>{service.serviceName}</th>
-              <td>{service.numClients.toLocaleString()}</td>
-              <td>{service.numInteractions.toLocaleString()}</td>
-              <td>{formatDuration(service.totalDuration)}</td>
+            <tr>
+              <th>End Date</th>
+              <td>
+                {data.reportParameters.end
+                  ? dayjs(data.reportParameters.end).format("MMM D, YYYY")
+                  : "\u2014"}
+              </td>
             </tr>
-          ))}
-        />
-      ))}
-      footerRows={
-        <tr>
-          <th>All programs</th>
-          <td>
-            <ToggleAllButton />
-          </td>
-          <td>{data.grandTotal.numClients.toLocaleString()}</td>
-          <td>{data.grandTotal.numInteractions}</td>
-          <td>{formatDuration(data.grandTotal.totalDuration)}</td>
-        </tr>
-      }
-    />
+          </>
+        }
+      />
+      <BasicTableReport
+        tableStyle={{ width: "100%" }}
+        headerRows={
+          <tr>
+            <th>Program</th>
+            <th style={{ width: "20%" }}>Service</th>
+            <th>Client Count</th>
+            <th>Interaction Count</th>
+            <th>Interaction Hours</th>
+          </tr>
+        }
+        contentRows={data.programs.map((program) => (
+          <CollapsibleTableRows
+            key={program.programId}
+            everpresentRow={
+              <tr>
+                <th>{program.programName}</th>
+                <td>
+                  <ToggleCollapseButton />
+                </td>
+                <td>{program.numClients.toLocaleString()}</td>
+                <td>{program.numInteractions.toLocaleString()}</td>
+                <td>{formatDuration(program.totalDuration)}</td>
+              </tr>
+            }
+            collapsibleRows={groupedServices[program.programId].map(
+              (service) => (
+                <tr key={service.serviceId}>
+                  <td>{"\u2014"}</td>
+                  <th>{service.serviceName}</th>
+                  <td>{service.numClients.toLocaleString()}</td>
+                  <td>{service.numInteractions.toLocaleString()}</td>
+                  <td>{formatDuration(service.totalDuration)}</td>
+                </tr>
+              )
+            )}
+          />
+        ))}
+        footerRows={
+          <tr>
+            <th>All programs</th>
+            <td>
+              <ToggleAllButton />
+            </td>
+            <td>{data.grandTotal.numClients.toLocaleString()}</td>
+            <td>{data.grandTotal.numInteractions}</td>
+            <td>{formatDuration(data.grandTotal.totalDuration)}</td>
+          </tr>
+        }
+      />
+    </>
   );
 }
