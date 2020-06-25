ea6784d241bb3c394358d93ce2c7a2907fc6d144
Carlos <filoxo@users.noreply.github.com>
2020-06-25 10:37:35 -0600

    Fix/321 (#565)
    * Fix useForceUpdate hook not triggering correctly
    
    * Use objects instead of boolean to force update

--- frontend/util/use-force-update.tsx
+++ frontend/util/use-force-update.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export function useForceUpdate() {
-  const [bool, setBool] = React.useState(false);
-
-  return () => setBool(!bool);
+  const [, setValue] = React.useState({});
+  return () => setValue({});
 }
