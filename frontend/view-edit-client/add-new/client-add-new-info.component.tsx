1e75d8309bca7e20ab89bf0d3d1d1d1c02efe6f0
Joel Denning <joeldenning@gmail.com>
2020-06-29 22:30:22 -0600

    Adding card for adding files to client page.

--- frontend/view-edit-client/add-new/client-add-new-info.component.tsx
+++ frontend/view-edit-client/add-new/client-add-new-info.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeCard from "../../home/home-card.component";
 import caseNoteUrl from "../../../icons/148705-essential-collection/svg/note.svg";
 import recordVisitUrl from "../../../icons/148705-essential-collection/svg/hourglass-2.svg";
+import filesUrl from "../../../icons/148705-essential-collection/svg/folder-19.svg";
 import { useCss } from "kremling";
 import { SingleClient } from "../view-client.component";
 
@@ -19,6 +20,11 @@ export default function ClientAddNewInfo(props: ClientAddNewInfoProps) {
         iconUrl={recordVisitUrl}
         title="Add client interaction"
         link={`/clients/${props.clientId}/add-client-interaction`}
+      />
+      <HomeCard
+        iconUrl={filesUrl}
+        title="Add files"
+        link={`/clients/${props.clientId}/files`}
       />
     </div>
   );
