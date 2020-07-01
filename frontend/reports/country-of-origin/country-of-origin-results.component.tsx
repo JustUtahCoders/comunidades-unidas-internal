6a9c2951a8068eb72cc10b78d66f8d10c709e312
Joel Denning <joeldenning@gmail.com>
2020-07-01 16:32:18 -0600

    Separate unknown countries of origin from known origins.

--- frontend/reports/country-of-origin/country-of-origin-results.component.tsx
+++ frontend/reports/country-of-origin/country-of-origin-results.component.tsx
@@ -15,6 +15,8 @@ export default function CountriesOfOriginResults(props) {
   }
 
   const totalClients = sum(values(data.countriesOfOrigin));
+  const unknownClients = data.countriesOfOrigin.Unknown;
+  const totalKnownOriginClients = totalClients - unknownClients;
 
   const sortedCountries = entries<number>(data.countriesOfOrigin).sort(
     (first, second) => {
@@ -23,35 +25,63 @@ export default function CountriesOfOriginResults(props) {
   );
 
   return (
-    <BasicTableReport
-      title="Client Countries of Origin"
-      headerRows={
-        <tr>
-          <th>Country</th>
-          <th>Client count</th>
-          <th>Percentage</th>
-        </tr>
-      }
-      contentRows={
-        <>
-          {sortedCountries.map((country) => (
-            <tr key={country[0]}>
-              <th>{countryCodeToName[country[0]] || country[0]}</th>
-              <td>{country[1].toLocaleString()}</td>
-              <td>{formatPercentage(country[1], totalClients)}</td>
+    <>
+      <BasicTableReport
+        title="Client Countries of Origin"
+        headerRows={null}
+        contentRows={
+          <>
+            <tr>
+              <td>Known</td>
+              <td>{totalKnownOriginClients}</td>
             </tr>
-          ))}
-        </>
-      }
-      footerRows={
-        <>
+            <tr>
+              <td>Unknown</td>
+              <td>{unknownClients}</td>
+            </tr>
+          </>
+        }
+        footerRows={
           <tr>
-            <th>Total</th>
-            <td>{totalClients}</td>
-            <td>100%</td>
+            <td>Total clients</td>
+            <td>{totalClients.toLocaleString()}</td>
           </tr>
-        </>
-      }
-    />
+        }
+      />
+      <BasicTableReport
+        title="Country breakdown"
+        headerRows={
+          <tr>
+            <th>Country</th>
+            <th>Client count</th>
+            <th>Percentage</th>
+          </tr>
+        }
+        contentRows={
+          <>
+            {sortedCountries
+              .filter((c) => c[0] !== "Unknown")
+              .map((country) => (
+                <tr key={country[0]}>
+                  <th>{countryCodeToName[country[0]] || country[0]}</th>
+                  <td>{country[1].toLocaleString()}</td>
+                  <td>
+                    {formatPercentage(country[1], totalKnownOriginClients)}
+                  </td>
+                </tr>
+              ))}
+          </>
+        }
+        footerRows={
+          <>
+            <tr>
+              <th>Total</th>
+              <td>{totalKnownOriginClients}</td>
+              <td>100%</td>
+            </tr>
+          </>
+        }
+      />
+    </>
   );
 }
