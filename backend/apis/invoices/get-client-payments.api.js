42d25ba510926469004c2fb3aad8dbd4734e6a0a
Joel Denning <joeldenning@gmail.com>
2020-08-24 14:40:19 -0600

    Self review

--- backend/apis/invoices/get-client-payments.api.js
+++ backend/apis/invoices/get-client-payments.api.js
@@ -41,8 +41,6 @@ app.get("/api/clients/:clientId/payments", (req, res) => {
       return databaseError(req, res, err);
     }
 
-    console.log(result, redactedTags);
-
     res.send({
       payments: result.map((payment) =>
         formatResponsePayment({
