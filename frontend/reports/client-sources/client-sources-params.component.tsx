56f5cac35d495fa479e96c9007d566edbd9b7497
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-09-12 11:20:05 -0600

    Add date range on dateOfIntake for client-sources

--- frontend/reports/client-sources/client-sources-params.component.tsx
+++ frontend/reports/client-sources/client-sources-params.component.tsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { useQueryParamState } from "../../util/use-query-param-state.hook";
 import { Link } from "@reach/router";
 
 export default function ClientSourcesParams(props) {
+  const [startDate, setStartDate] = useQueryParamState("start", "");
+  const [endDate, setEndDate] = useQueryParamState("end", "");
+
   return (
-    <div className="actions">
-      <Link
-        className="primary button"
-        to={`${window.location.pathname}/results${window.location.search}`}
-      >
-        Run report
-      </Link>
-    </div>
+    <>
+      <div className="report-input">
+        <label htmlFor="start-date">Start date:</label>
+        <input
+          id="start-date"
+          type="date"
+          value={startDate}
+          onChange={(evt) => setStartDate(evt.target.value)}
+        />
+      </div>
+      <div className="report-input">
+        <label htmlFor="end-date">End date:</label>
+        <input
+          id="end-date"
+          type="date"
+          value={endDate}
+          onChange={(evt) => setEndDate(evt.target.value)}
+        />
+      </div>
+      <div className="actions">
+        <Link
+          className="primary button"
+          to={`${window.location.pathname}/results${window.location.search}`}
+        >
+          Run report
+        </Link>
+      </div>
+    </>
   );
 }
--- frontend/reports/client-sources/client-sources-results.component.tsx
+++ frontend/reports/client-sources/client-sources-results.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useReportsApi } from "../shared/use-reports-api";
 import BasicTableReport from "../shared/basic-table-report.component";
+import dayjs from "dayjs";
 import { formatPercentage, capitalize } from "../shared/report.helpers";
 import { sum, values, entries } from "lodash-es";
 import { clientSources } from "../../add-client/form-inputs/client-source-inputs.component";
@@ -23,35 +24,72 @@ export default function ClientSourcesResults(props) {
   );
 
   return (
-    <BasicTableReport
-      title="Client Sources"
-      headerRows={
-        <tr>
-          <th>Source</th>
-          <th>Client count</th>
-          <th>Percentage</th>
-        </tr>
-      }
-      contentRows={
-        <>
-          {sortedSources.map((source) => (
-            <tr key={source[0]}>
-              <th>{clientSources[source[0]] || source[0]}</th>
-              <td>{source[1].toLocaleString()}</td>
-              <td>{formatPercentage(source[1], totalClients)}</td>
-            </tr>
-          ))}
-        </>
-      }
-      footerRows={
-        <>
+    <>
+      <BasicTableReport
+        title="Client Date Of Intake Range"
+        headerRows={
           <tr>
-            <th>Total</th>
-            <td>{totalClients}</td>
-            <td>100%</td>
+            <th>Parameter</th>
+            <th>Value</th>
           </tr>
-        </>
-      }
-    />
+        }
+        contentRows={
+          <>
+            <tr>
+              <th>Start Date</th>
+              <td>
+                {data.reportParameters.start
+                  ? dayjs(data.reportParameters.start).format("MMM D, YYYY")
+                  : "\u2014"}
+              </td>
+            </tr>
+            <tr>
+              <th>End Date</th>
+              <td>
+                {data.reportParameters.end
+                  ? dayjs(data.reportParameters.end).format("MMM D, YYYY")
+                  : "\u2014"}
+              </td>
+            </tr>
+          </>
+        }
+        footerRows={
+          <tr>
+            <th>Total Clients</th>
+            <td>{totalClients.toLocaleString()}</td>
+          </tr>
+        }
+      />
+      <BasicTableReport
+        title="Client Sources"
+        headerRows={
+          <tr>
+            <th>Source</th>
+            <th>Client count</th>
+            <th>Percentage</th>
+          </tr>
+        }
+        contentRows={
+          <>
+            {sortedSources.map((source) => (
+              <tr key={source[0]}>
+                <th>{clientSources[source[0]] || source[0]}</th>
+                <td>{source[1].toLocaleString()}</td>
+                <td>{formatPercentage(source[1], totalClients)}</td>
+              </tr>
+            ))}
+          </>
+        }
+        footerRows={
+          <>
+            <tr>
+              <th>Total</th>
+              <td>{totalClients}</td>
+              <td>100%</td>
+            </tr>
+          </>
+        }
+      />
+    </>
   );
 }
