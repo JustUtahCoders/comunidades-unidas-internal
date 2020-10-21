1064b61e662ff518c89858e2fde9c15addaa4ba7
asekito <email@email.com>
2020-10-20 19:48:48 -0700

    changed so getting invoice sql gets all clients for that invoice

--- backend/apis/invoices/get-invoice.sql
+++ backend/apis/invoices/get-invoice.sql
@@ -1,31 +1,46 @@
-SELECT
-  invoices.id, invoices.invoiceNumber, invoices.invoiceDate, invoices.clientNote,
-  invoices.totalCharged, invoices.status, invoices.dateAdded, invoices.addedBy,
-  invoices.dateModified, invoices.modifiedBy, addedUser.id addedUserId,
-  addedUser.firstName addedFirstName, addedUser.lastName addedLastName,
-  modifiedUser.id modifiedUserId, modifiedUser.firstName modifiedUserFirstName,
+SELECT invoices.id,
+  invoices.invoiceNumber,
+  invoices.invoiceDate,
+  invoices.clientNote,
+  invoices.totalCharged,
+  invoices.status,
+  invoices.dateAdded,
+  invoices.addedBy,
+  invoices.dateModified,
+  invoices.modifiedBy,
+  addedUser.id addedUserId,
+  addedUser.firstName addedFirstName,
+  addedUser.lastName addedLastName,
+  modifiedUser.id modifiedUserId,
+  modifiedUser.firstName modifiedUserFirstName,
   modifiedUser.lastName modifiedUserLastName
-
-  FROM invoices
+FROM invoices
   JOIN users addedUser ON invoices.addedBy = addedUser.id
   JOIN users modifiedUser ON invoices.modifiedBy = modifiedUser.id
 WHERE invoices.id = ?;
-
-SELECT * FROM invoiceLineItems WHERE invoiceId = ?;
-
-SELECT * FROM invoiceClients WHERE invoiceId = ?;
-
-SELECT
-  payments.id, payments.paymentDate, payments.paymentAmount, payments.paymentType,
-  payments.donationId, payments.dateAdded, payments.addedBy, payments.dateModified,
-  payments.modifiedBy, invoicePayments.amount amountTowardsInvoice
-  FROM payments
+SELECT *
+FROM invoiceLineItems
+WHERE invoiceId = ?;
+SELECT clientId
+FROM invoiceClients
+WHERE invoiceId = ?;
+SELECT payments.id,
+  payments.paymentDate,
+  payments.paymentAmount,
+  payments.paymentType,
+  payments.donationId,
+  payments.dateAdded,
+  payments.addedBy,
+  payments.dateModified,
+  payments.modifiedBy,
+  invoicePayments.amount amountTowardsInvoice
+FROM payments
   JOIN invoicePayments ON invoicePayments.paymentId = payments.id
   JOIN invoices ON invoices.id = invoicePayments.invoiceId
-  WHERE invoices.id = ?;
-
-SELECT id, foreignId, tag
-  FROM tags
-  WHERE foreignTable = "invoices"
-  AND foreignId = ?
-;
+WHERE invoices.id = ?;
+SELECT id,
+  foreignId,
+  tag
+FROM tags
+WHERE foreignTable = "invoices"
+  AND foreignId = ?;
