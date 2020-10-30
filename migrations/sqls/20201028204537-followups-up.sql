8cf7ac10e5ee1236090a92128a1e598261875b3b
Andrea Sekito <58274693+asekito@users.noreply.github.com>
2020-10-30 10:25:04 -0700

    Followups schema (#674)
    * API post and patch request contract for follow ups
    
    * pluralized endpoint, follow up date made appointment date, and made a quick note describing what is required for the post request
    
    * notes section describing  what fields are required for the post request
    
    * initial up and down sql file for follow ups
    
    * create join table for follow up and services, added client id removed serviceId from schema
    
    * date of contact required and added MySQL semicolons syntax

---
+++ migrations/sqls/20201028204537-followups-up.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS followUps (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  clientId INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description MEDIUMTEXT,
+  dateOfContact DATETIME NOT NULL,
+  appointmentDate DATETIME,
+  addedBy INT NOT NULL,
+  updatedBy INT NOT NULL,
+  FOREIGN KEY (clientId) REFERENCES clients(id),
+  FOREIGN KEY (addedBy) REFERENCES users(id),
+  FOREIGN KEY (updatedBy) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS followUpServices (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  serviceId INT NOT NULL,
+  followUpId INT NOT NULL,
+  FOREIGN KEY (serviceId) REFERENCES services(id),
+  FOREIGN KEY (followUpId) REFERENCES followUps(id)
+);
---
+++ migrations/sqls/20201028205336-followups-down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS followUps;
+DROP TABLE IF EXISTS followUpServices;
