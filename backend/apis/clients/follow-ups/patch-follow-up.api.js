b49f93cca8b0f1ff9f11a7979862606040f5b931
asekito <dresekito@gmail.com>
2020-11-05 18:50:29 -0700

    update sql queries for followUps table

--- backend/apis/clients/follow-ups/patch-follow-up.api.js
+++ backend/apis/clients/follow-ups/patch-follow-up.api.js
@@ -22,6 +22,7 @@ app.patch("/api/clients/:clientId/follow-ups/:followUpId", (req, res) => {
   // validations here
 
   const { clientId, followUpId } = req.params;
+
   const {
     serviceIds,
     title,
@@ -39,7 +40,40 @@ app.patch("/api/clients/:clientId/follow-ups/:followUpId", (req, res) => {
     if (err) {
       databaseError(req, res, err);
     }
-    console.log(followUpResult);
+    const getCurrentUserSql = `SELECT JSON_OBJECT("userId", users.id, "firstName", users.firstName, "lastName", users.lastName) lastUpdatedBy FROM users WHERE id = ${user.id}`;
+    pool.query(getCurrentUserSql, (err, userResult) => {
+      if (err) {
+        return databaseError(req, res, err);
+      }
+      const newFollowUpInfo = { ...req.body, ...userResult[0] };
+      const newFollowUp = Object.assign({}, followUpResult[0], newFollowUpInfo);
+      const updateFollowUpSql = mysql.format(
+        `UPDATE followUps SET
+          title = ?,
+          description = ?,
+          dateOfContact = ?,
+          appointmentDate = ?,
+          updatedBy = ?
+        WHERE id = ?
+      `,
+        [
+          newFollowUp.title,
+          newFollowUp.description,
+          newFollowUp.dateOfContact,
+          newFollowUp.appointmentDate,
+          newFollowUp.lastUpdatedBy,
+          newFollowUp.id,
+        ]
+      );
+
+      pool.query(updateFollowUpSql, (err, updateResult) => {
+        if (err) {
+          return databaseError(req, res, err);
+        }
+        console.log(updateResult);
+        // foreign keys constraints can turn off check, delete from join table at fu id and then add new/updated ones too?
+      });
+    });
   });
 
   res.send("hit");
