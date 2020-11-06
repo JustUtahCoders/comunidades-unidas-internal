9520afc4fd0700f2a1d759a6072c447216268072
asekito <dresekito@gmail.com>
2020-11-05 18:20:49 -0700

    querying db and getting follow up by id

--- backend/apis/clients/follow-ups/patch-follow-up.api.js
+++ backend/apis/clients/follow-ups/patch-follow-up.api.js
@@ -11,3 +11,36 @@ const {
 } = require("../../utils/validation-utils");
 const fs = require("fs");
 const path = require("path");
+const getFollowUpSql = fs.readFileSync(
+  path.resolve(__dirname, "./get-follow-up.sql"),
+  "utf-8"
+);
+
+app.patch("/api/clients/:clientId/follow-ups/:followUpId", (req, res) => {
+  const user = req.session.passport.user;
+
+  // validations here
+
+  const { clientId, followUpId } = req.params;
+  const {
+    serviceIds,
+    title,
+    description,
+    dateOfContact,
+    appointmentDate,
+  } = req.body;
+
+  const getFollowUpByIdSql = mysql.format(getFollowUpSql, [
+    followUpId,
+    followUpId,
+  ]);
+
+  pool.query(getFollowUpByIdSql, (err, followUpResult) => {
+    if (err) {
+      databaseError(req, res, err);
+    }
+    console.log(followUpResult);
+  });
+
+  res.send("hit");
+});
