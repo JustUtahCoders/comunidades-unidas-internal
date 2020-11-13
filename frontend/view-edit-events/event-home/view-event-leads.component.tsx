a775de410a553db9b6cbdea55bf87c6aeaec29c0
Justin McMurdie <mcmurdiej@gmail.com>
2020-11-13 15:59:38 -0700

    Adding Event Leads to UI when looking at a specific event

---
+++ frontend/view-edit-events/event-home/view-event-leads.component.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SingleEvent } from "../view-event.component";
+import EventSection from "./event-section.component";
+import BasicTableReport from "../../reports/shared/basic-table-report.component";
+import { Link } from "@reach/router";
+
+export default function ViewEventStats(props: ViewEventStatsProps) {
+  const { leads } = props.event;
+
+  return (
+    <EventSection title="Leads">
+      <BasicTableReport
+        headerRows={
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        }
+        contentRows={
+          <>
+            {leads.map((lead) => (
+              <tr>
+                <td>
+                  <Link to={`/leads/${lead.id}`}>{lead.id}</Link>
+                </td>
+                <td>{lead.fullName}</td>
+              </tr>
+            ))}
+          </>
+        }
+      />
+    </EventSection>
+  );
+}
+
+type ViewEventStatsProps = {
+  event: SingleEvent;
+};
