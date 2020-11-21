ff30077f149bff28a59b20d34093d5049ad68b0c
Andrea Sekito <58274693+asekito@users.noreply.github.com>
2020-11-21 11:47:07 -0700

    Followup patch endpoint (#692)
    * basic outlining post request and updated API contract
    
    * basic insert sql for follow ups and added .api to post request file
    
    * setting up query and request items to use for query
    
    * validation of request body and params
    
    * insertion queries for followUps and followUpServices tables
    
    * fleshed out response for post request
    
    * post request response
    
    * removed unnecessary console log
    
    * patch api file created
    
    * removed unused variable
    
    * querying db and getting follow up by id
    
    * update sql queries for followUps table
    
    * updating both followUps and followUpServices table information
    
    * response object after updating follow up
    
    * parsing JSON from result of query and security fix with sql
    
    * prepared sql statements prevent injections
    
    * spelling fix
    
    * requiring patch api
    
    * remove unnecessary db query
    
    * typo fix and remove console log
    
    * removal of unnecessary user query

---
+++ backend/apis/clients/follow-ups/patch-follow-up.api.js
@@ -0,0 +1,94 @@
+const { app, databaseError, pool, invalidRequest } = require("../../../server");
+const mysql = require("mysql");
+const {
+  checkValid,
+  validId,
+  validDate,
+  validTime,
+  validArray,
+  validDateTime,
+  nullableValidDateTime,
+} = require("../../utils/validation-utils");
+const fs = require("fs");
+const path = require("path");
+const getFollowUpSql = fs.readFileSync(
+  path.resolve(__dirname, "./get-follow-up.sql"),
+  "utf-8"
+);
+
+app.patch("/api/clients/:clientId/follow-ups/:followUpId", (req, res) => {
+  const user = req.session.passport.user;
+
+  const validationErrors = [
+    ...checkValid(req.params, validId("clientId"), validId("followUpId")),
+    ...checkValid(req.body, validArray("serviceIds", validId)),
+    ...checkValid(req.body, validDateTime("dateOfContact")),
+    ...checkValid(req.body, nullableValidDateTime("appointmentDate")),
+  ];
+
+  const { clientId, followUpId } = req.params;
+
+  const {
+    serviceIds,
+    title,
+    description,
+    dateOfContact,
+    appointmentDate,
+  } = req.body;
+
+  const getFollowUpByIdSql = mysql.format(getFollowUpSql, [
+    followUpId,
+    followUpId,
+  ]);
+
+  pool.query(getFollowUpByIdSql, (err, followUpResult) => {
+    if (err) {
+      databaseError(req, res, err);
+    }
+
+    const newFollowUpInfo = { ...req.body };
+    const newFollowUp = Object.assign({}, followUpResult[0], newFollowUpInfo);
+
+    let updateFollowUpSql = mysql.format(
+      `UPDATE followUps SET
+          title = ?,
+          description = ?,
+          dateOfContact = ?,
+          appointmentDate = ?,
+          updatedBy = ?
+        WHERE id = ?;
+      `,
+      [
+        newFollowUp.title,
+        newFollowUp.description,
+        newFollowUp.dateOfContact,
+        newFollowUp.appointmentDate,
+        user.id,
+        newFollowUp.id,
+      ]
+    );
+    const oldServiceIds = JSON.parse(followUpResult[0].serviceIds);
+    updateFollowUpSql =
+      mysql.format("DELETE FROM followUpServices WHERE followUpId = ?;", [
+        newFollowUp.id,
+      ]) + updateFollowUpSql;
+    newFollowUp.serviceIds.forEach((id) => {
+      updateFollowUpSql += mysql.format(
+        "INSERT INTO followUpServices (serviceId, followUpId) VALUES (?, ?);",
+        [id, newFollowUp.id]
+      );
+    });
+    pool.query(updateFollowUpSql, (err, updateResult) => {
+      if (err) {
+        return databaseError(req, res, err);
+      }
+
+      pool.query(getFollowUpByIdSql, (err, followUp) => {
+        if (err) {
+          return databaseError(req, res, err);
+        }
+        res.send(followUp);
+      });
+    });
+  });
+});
