f1df9f19bc36b79b55f9ea3f4ecdeb52bdbdaa2f
Joel Denning <joeldenning@gmail.com>
2020-11-23 17:51:57 -0700

    Fix bug where JPLS integration said it worked when it didn't.

--- backend/apis/clients/integrations/jpls-integration.js
+++ backend/apis/clients/integrations/jpls-integration.js
@@ -1,18 +1,24 @@
 const { upsertClient } = require("juntos-por-la-salud-node-client");
 
 module.exports = function juntosPorLaSalud(integration, client) {
+  console.log("upserting client to jpls");
   return upsertClient({
     client,
     participantId: integration.externalId,
     username: process.env.JPLS_USERNAME,
     password: process.env.JPLS_PASSWORD,
-  }).then(
-    (participantId) => ({
-      error: null,
-      externalId: participantId,
-    }),
-    (error) => ({
-      error,
+  })
+    .then((participantId) => {
+      if (!participantId) {
+        throw Error("No participant id found after upsert");
+      } else {
+        return {
+          error: null,
+          externalId: participantId,
+        };
+      }
     })
-  );
+    .catch((error) => ({
+      error,
+    }));
 };
