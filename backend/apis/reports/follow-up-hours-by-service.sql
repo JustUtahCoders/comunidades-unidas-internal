156a8680d88cdeb4e12097b74d4ecfcea9b8cb6e
asekito <dresekito@gmail.com>
2020-11-30 21:24:33 -0700

    drafting out sql for follow ups (this is just saving the work)

---
+++ backend/apis/reports/follow-up-hours-by-service.sql
@@ -0,0 +1,7 @@
+-- holding this query for future purpose
+-- num of clients per follow up
+--  SELECT clientId, totalInteractions, services.id servicId, services.serviceName, programs.id programId, programs.programName FROM services LEFT OUTER JOIN (SELECT COUNT(*) totalInteractions, serviceId, followUpId FROM followUpServices GROUP BY serviceId) numInteractions ON services.id = numInteractions.serviceId JOIN programs ON programs.id = services.programId LEFT OUTER JOIN (SELECT id, clientId FROM followUps) f ON numInteractions.followUpId = f.id
+
+
+-- num hours per service
+-- SELECT  SUM(TIME_TO_SEC(duration)) totalInteractionSeconds, followUpServices.serviceId serviceId FROM followUps JOIN clients ON clients.id = followUps.clientId  JOIN followUpServices ON followUpServices.followUpId = followUps.id GROUP BY followUpServices.serviceId;
